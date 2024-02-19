044ba2cfde5ea65e2c1c176b9806a5b02dd45688
Abrham <abrsh6266@gmail.com>
2024-02-18 20:55:51 -0900

    defining function to handle the text to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import Form from "./Form";
 
 const App = () => {
-  const [count,setCount] = useState(0)
-  return <h2>Lorem Ipsum Starter</h2>;
-  
+  const [count, setCount] = useState(0);
+  console.log(count);
+  return (
+    <section className="section-center">
+      <h4>Tired of Lorem Ipsum</h4>
+      <Form count={count} setCount={setCount} />
+    </section>
+  );
 };
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import data from "./data";
 
-const Form = () => {
+const Form = ({ count, setCount }) => {
+  const [text, setText] = useState(data);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const tex = data.slice(0, count);
+    setText(tex);
+  };
   return (
-    <div>Form</div>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="count" className="label">
+          Paragraphs
+        </label>
+        <input
+          type="text"
+          className="input"
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+          id="count"
+          value={count}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+    </>
+  );
+};
 
-export default Form
+export default Form;
